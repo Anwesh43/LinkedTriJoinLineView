582d3946f59235668d8c2efe8e967462fe86bf80
Anwesh43 <anweshthecool0@gmail.com>
2019-05-16 02:38:37 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedtrijoinlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrijoinlineview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedtrijoinlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.trijoinlineview.TriJoinLineView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TriJoinLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
