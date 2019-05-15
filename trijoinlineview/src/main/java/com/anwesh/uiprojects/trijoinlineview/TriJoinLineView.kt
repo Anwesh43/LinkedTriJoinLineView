5965e07a5b5e924ae5a02194c4c39e4d513802ad
Anwesh43 <anweshthecool0@gmail.com>
2019-05-16 02:36:01 +0530

    adding static create method

--- trijoinlineview/src/main/java/com/anwesh/uiprojects/trijoinlineview/TriJoinLineView.kt
+++ trijoinlineview/src/main/java/com/anwesh/uiprojects/trijoinlineview/TriJoinLineView.kt
@@ -215,4 +215,13 @@ class TriJoinLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : TriJoinLineView {
+            val view : TriJoinLineView = TriJoinLineView(activity)
+            activity.setContentView(view)
+            return view 
+        }
+    }
 }
