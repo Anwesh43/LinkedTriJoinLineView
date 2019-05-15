de03e625cae57b9516eb22cd8b2871ec00055aae
Anwesh43 <anweshthecool0@gmail.com>
2019-05-16 02:37:54 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtrijoinlineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtrijoinlineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtrijoinlineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.trijoinlineview.TriJoinLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriJoinLineView.create(this)
     }
 }
