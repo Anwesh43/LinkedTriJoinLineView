155499761c27c28bdaaa2c5fc0f305394ba2ec40
Anwesh43 <anweshthecool0@gmail.com>
2019-05-16 01:34:19 +0530

    adding libraries and constants

--- trijoinlineview/src/main/java/com/anwesh/uiprojects/trijoinlineview/TriJoinLineView.kt
+++ trijoinlineview/src/main/java/com/anwesh/uiprojects/trijoinlineview/TriJoinLineView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.trijoinlineview
 /**
  * Created by anweshmishra on 16/05/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
